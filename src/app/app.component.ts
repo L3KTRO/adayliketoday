d84089e344b232f5d5e9f397cd39eabcb4e60751
Lolo Escobar. <bylectroyt@gmail.com>
2025-03-20 20:47:48 +0100

    before ng19 update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import {FooterComponent} from "./footer/footer.component";
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    HttpClientModule,
+    
+// TODO: `HttpClientModule` should not be imported into a component directly.
+// Please refactor the code to add `provideHttpClient()` call to the provider list in the
+// application bootstrap logic and remove the `HttpClientModule` import from this component.
+HttpClientModule,
     FontAwesomeModule,
     HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
